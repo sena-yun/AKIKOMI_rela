306fc5b7b29d121e0576377cbc10c2260b1482c7
田中大誓 <2020m062@kuas.ac.jp>
2021-06-07 19:33:28 +0900

    Progress

--- AKIKOMI_REAL/ViewController.swift
+++ AKIKOMI_REAL/ViewController.swift
@@ -6,13 +6,18 @@
 //
 
 import UIKit
+import MBCircularProgressBar
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var Circular_Progressbar_Library: MBCircularProgressBarView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
     
+    @IBAction func Progress_Bar_Library(_ sender: Any) {
+    }
 }
 
