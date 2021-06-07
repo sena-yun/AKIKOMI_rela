323f220da71528ba8c41f298cd39ab55a7a76481
Sena Yun <2020m091@kuas.ac.jp>
2021-06-07 17:40:23 +0900

    fuck me

---
+++ AKIKOMI_REAL/InfoViewController.swift
@@ -0,0 +1,35 @@
+//
+//  InfoViewController.swift
+//  AKIKOMI_REAL
+//
+//  Created by 尹セナ on 2021/05/25.
+//
+
+import UIKit
+
+class InfoViewController: UIViewController {
+
+    @IBOutlet weak var InfoSegment: UISegmentedControl!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func InfoSegmentAction(_ sender: UISegmentedControl) {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
