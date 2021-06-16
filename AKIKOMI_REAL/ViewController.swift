98d32311e27589376ea9d4630c659c9066e4d066
田中大誓 <2020m062@kuas.ac.jp>
2021-06-16 15:57:20 +0900

    URLを追加

--- AKIKOMI_REAL/ViewController.swift
+++ AKIKOMI_REAL/ViewController.swift
@@ -15,5 +15,20 @@ class ViewController: UIViewController {
         
     }
     
+    @IBAction func URLcm1(_ sender: UIButton) {
+        UIApplication.shared.open(URL(string:"https://www.kuas.ac.jp/opencampus")! as URL, options: [:], completionHandler: nil)
+    }
+    
+    @IBAction func URLcm2(_ sender: UIButton) {
+        UIApplication.shared.open(URL(string:"https://www.kuas.ac.jp/opencampus/2021/202105-web#online")! as URL, options: [:], completionHandler: nil)
+    }
+    
+    @IBAction func URLcm3(_ sender: UIButton) {
+        UIApplication.shared.open(URL(string:"https://www.kuas.ac.jp/interview/")! as URL, options: [:], completionHandler: nil)
+    }
+    
+    @IBAction func URLcm4(_ sender: UIButton) {
+        UIApplication.shared.open(URL(string:"https://www.kuas.ac.jp/opencampus/2021/webopencampus")! as URL, options: [:], completionHandler: nil)
+    }
 }
 
